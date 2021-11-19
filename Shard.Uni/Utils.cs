70f6c1c53a1a8b9e0a2f5276b4fd53aead91147f
azerpas <anthony@smbs.io>
2021-11-19 10:48:33 +0100

    Added method Capitalize

---
+++ Shard.Uni/Utils.cs
@@ -0,0 +1,13 @@
+﻿namespace Shard.Uni
+{
+    public static class Utils
+    {
+        public static class Strings
+        {
+            public static string Capitalize(string word)
+            {
+                return word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+            }
+        }
+    }
+}
