af73fa6e66aeda983b3846f0395a6fa784ff7db7
azerpas <anthony@smbs.io>
2021-12-10 11:12:37 +0100

    Init Unit Repository

---
+++ Shard.Uni/Repositories/IUnitRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shard.Uni.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shard.Uni.Repositories
+{
+    public interface IUnitRepository
+    {
+        void RegisterClassMap();
+        Task<IReadOnlyList<Unit>> GetUnits();
+        Task<Unit> GetUnitById(string id);
+    }
+}
---
+++ Shard.Uni/Repositories/UnitRepository.cs
@@ -0,0 +1,24 @@
+﻿using Shard.Uni.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shard.Uni.Repositories
+{
+    public class UnitRepository : IUnitRepository
+    {
+        Task<Unit> IUnitRepository.GetUnitById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        Task<IReadOnlyList<Unit>> IUnitRepository.GetUnits()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegisterClassMap()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
