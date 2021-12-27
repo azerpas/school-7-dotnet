acc7a6881df8b311849c0679ae62fee6c6f95b5a
azerpas <anthony@smbs.io>
2021-12-27 16:32:55 +0100

    Modified Unit Repository

--- Shard.Uni/Repositories/UnitRepository.cs
+++ Shard.Uni/Repositories/UnitRepository.cs
@@ -1,4 +1,10 @@
-﻿using Shard.Uni.Models;
+﻿using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Shard.Shared.Core;
+using Shard.Uni.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,19 +12,42 @@ namespace Shard.Uni.Repositories
 {
     public class UnitRepository : IUnitRepository
     {
-        Task<Unit> IUnitRepository.GetUnitById(string id)
+        private readonly IMongoCollection<Unit> collection;
+        private readonly ILogger<UnitRepository> logger;
+
+        public UnitRepository(MongoConnector mongo, ILogger<UnitRepository> logger)
         {
-            throw new System.NotImplementedException();
+            RegisterClassMap();
+
+            collection = mongo.Connector.GetCollection<Unit>("unit");
+            this.logger = logger;
         }
 
-        Task<IReadOnlyList<Unit>> IUnitRepository.GetUnits()
+        async Task<Unit> IUnitRepository.GetUnitById(string id)
         {
-            throw new System.NotImplementedException();
+            return await(await collection.FindAsync(id)).SingleAsync();
+        }
+
+        async Task<IReadOnlyList<Unit>> IUnitRepository.GetUnits()
+        {
+            return await(await collection.FindAsync(Builders<Unit>.Filter.Empty)).ToListAsync();
         }
 
         public void RegisterClassMap()
         {
-            throw new System.NotImplementedException();
+            BsonClassMap.RegisterClassMap<Unit>(map =>
+            {
+                map.AutoMap();
+                map.MapIdProperty(unit => unit.Id);
+                map.MapProperty(unit => unit.Type);
+                map.MapProperty(unit => unit.Planet);
+                map.MapProperty(unit => unit.System);
+                map.MapProperty(unit => unit.DestinationPlanet);
+                map.MapProperty(unit => unit.DestinationSystem);
+                map.MapProperty(unit => unit.DestinationShard);
+                map.MapProperty(unit => unit.EstimatedTimeOfArrival);
+            });
+            BsonSerializer.RegisterSerializer(new EnumSerializer<ResourceKind>(BsonType.String));
         }
     }
 }
